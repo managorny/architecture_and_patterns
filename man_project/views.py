effb3ba1b5f870550061e87c013a6d002cfaa2b6
solunasalles <m.a.nagorny@gmail.com>
2021-02-24 09:25:41 +0500

    homework02 - processing request_method 'post' - get data from request_method 'post' - get data from request_method 'get' - add contact page with request_method 'post' - save message from users into file and print in Terminal

--- man_project/views.py
+++ man_project/views.py
@@ -10,6 +10,9 @@ def main_view(request):
         'page_name': 'Main',
         'page_header': 'Main_page',
     }
+    if request['method'] == 'GET':
+        context['request_params'] = request['request_params']
+        context['method'] = request['method']
     return '200 OK', render('index.html', fc_request=fc_request, context=context)
 
 
@@ -29,3 +32,21 @@ def err_view(request):
         'page_header': '404_page',
     }
     return '404 Not Found', render('404.html', fc_request=fc_request, context=context)
+
+
+def contact_view(request):
+    context = {
+        'page_name': 'Обратная связь',
+        'page_header': 'Обратная связь',
+    }
+    if request['method'] == 'POST':
+        data = request['data']
+        title = data['title']
+        text = data['text']
+        email = data['email']
+        with open('messages.txt', 'a') as f:
+            f.write(f'Пришло сообщение от {email}. Тема: {title}. Текст: {text}\n')
+        print(f'Пришло сообщение от {email}. Тема: {title}. Текст: {text}\n')
+        return '200 OK', render('contact_us.html', context=context)
+    else:
+        return '200 OK', render('contact_us.html', context=context)
--- study_framework/core.py
+++ study_framework/core.py
@@ -10,6 +10,19 @@ class Application:
     def __call__(self, env, first_response):
         path = env['PATH_INFO']
         request = {'path': path}
+
+        # Получаем все данные запроса
+        method = env['REQUEST_METHOD']
+        request_data = self.get_wsgi_input_data(env)
+
+        query_string = env['QUERY_STRING']
+        request_params = self.parse_input_data(query_string)
+
+        # добавляем параметры запросов
+        request['method'] = method
+        request['data'] = request_data
+        request['request_params'] = request_params
+
         for controller in self.front_controllers:
             controller(request)
             if 'path' in request and 'main' in request:
@@ -19,7 +32,6 @@ class Application:
             view = self.urlpatterns[path]
 
             status_code, text = view(request)
-            print(status_code, text)
             first_response(status_code, [('Content-Type', 'text/html')])
             return [text.encode('utf-8')]
         else:
@@ -28,3 +40,47 @@ class Application:
             status_code = '404 Not Found'
             first_response(status_code, [('Content-Type', 'text/html')])
             return [b'404 Not Found']
+
+    @staticmethod
+    def parse_input_data(data: str):
+        """
+        :param data: принимает декодированну строку входящих данных в запросе
+        :return: возвращает словарь
+        """
+        result = {}
+        if data:
+            params = data.split('&')
+
+            for item in params:
+                key, value = item.split('=')
+                result[key] = value
+        return result
+
+    def parse_wsgi_input_data(self, data: bytes):
+        """
+        :param data: принимает байты данных в запросе
+        :return: возвращает словарь
+        """
+        result = {}
+        if data:
+            data_str = data.decode(encoding='utf-8')
+            result = self.parse_input_data(data_str)
+        return result
+
+    def get_wsgi_input_data(self, env):
+        """
+        :param env: получает данные из запроса, если они есть
+        :return: словарь
+        """
+        content_length_data = env.get('CONTENT_LENGTH')
+        if content_length_data:
+            content_length = int(content_length_data)
+        else:
+            content_length = 0
+        # content_length = int(content_length_data) if content_length_data else 0
+        if content_length > 0:
+            data = env['wsgi.input'].read(content_length)
+        else:
+            data = b''
+        # data = env['wsgi.input'].read(content_length) if content_length > 0 else b''
+        return self.parse_wsgi_input_data(data)
